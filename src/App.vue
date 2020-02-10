f6dbc6acfcde6160e4c683286b7ddf40bed9a2ac
bdronneau <basti1.dr@gmail.com>
2020-02-10 13:00:19 +0100

    refactor(list): add about section

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,15 @@
             <v-list-item-title>Settings</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item 
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
           v-on:click="disconnect()"
           link
         >
