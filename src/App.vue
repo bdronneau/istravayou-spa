8ab48fa6ba7b52eb0e6ae20d1a2de1eff9ed87d0
bdronneau <basti1.dr@gmail.com>
2020-02-11 12:38:08 +0100

    chore: indent

--- src/App.vue
+++ src/App.vue
@@ -5,73 +5,73 @@
       app
       clipped
     >
-      <v-list dense>
-        <v-list-item link v-on:click="resetNav()" to="dashboard">
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link v-on:click="resetNav()" to="account">
-          <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item
-          v-on:click="disconnect()"
-          link
-        >
-          <v-list-item-action>
-            <v-icon>mdi-logout</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Logout</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <v-list dense>
+      <v-list-item link v-on:click="resetNav()" to="dashboard">
+        <v-list-item-action>
+          <v-icon>mdi-view-dashboard</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Dashboard</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item link v-on:click="resetNav()" to="account">
+        <v-list-item-action>
+          <v-icon>mdi-settings</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Settings</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item
+        v-on:click="disconnect()"
+        link
+      >
+      <v-list-item-action>
+        <v-icon>mdi-logout</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</v-navigation-drawer>
 
-    <v-app-bar
-      app
-      clipped-left
+<v-app-bar
+  app
+  clipped-left
+>
+<v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+<router-link class="home_link" :to="{ path: '/'}">
+  <v-toolbar-title>I Strava You</v-toolbar-title>
+</router-link>
+</v-app-bar>
+
+<v-content>
+  <v-container>
+    <v-row
+      v-if="isLoading"
+      align="center"
+      justify="center"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-        <router-link class="home_link" :to="{ path: '/'}">
-          <v-toolbar-title>I Strava You</v-toolbar-title>
-        </router-link>
-    </v-app-bar>
+    <v-progress-circular
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
+    We have lift off
+  </v-row>
 
-    <v-content>
-      <v-container>
-        <v-row
-          v-if="isLoading"
-          align="center"
-          justify="center"
-        >
-            <v-progress-circular
-              indeterminate
-              color="primary"
-            ></v-progress-circular>
-            We have lift off
-        </v-row>
+  <router-view v-if="!isLoading" />
+</v-container>
+</v-content>
 
-        <router-view v-if="!isLoading" />
-      </v-container>
-    </v-content>
-
-    <v-footer app>
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+<v-footer app>
+  <span>&copy; 2019</span>
+</v-footer>
+</v-app>
 </template>
 
 <script>
-import { getStorageValue, cleanStorageValue } from './helpers/localstorage';
+  import { getStorageValue, cleanStorageValue } from './helpers/localstorage';
   export default {
     props: {
       source: String,
